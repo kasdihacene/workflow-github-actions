7160a2bdf33db5ac51eef371141bda8854d0e2ac
Hacene <hacene.upmc@gmail.com>
2020-04-29 16:02:04 +0200

    100% coverage

---
+++ src/test/java/com/workflow/github/WorkflowApplicationTest.java
@@ -0,0 +1,11 @@
+package com.workflow.github;
+
+import org.junit.jupiter.api.Test;
+
+class WorkflowApplicationTest {
+
+    @Test
+    public void main() {
+        WorkflowApplication.main(new String[]{});
+    }
+}
